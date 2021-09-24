7412d9c508a0ee49e6b10b34cde93af4ef52e6bb
Nguyen Phuong Nam <namnp.201002@gmail.com>
2021-09-24 16:37:49 +0700

    update lan 6

--- CPP0501.cpp
+++ CPP0501.cpp
@@ -1,21 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-struct Point{
-	double x, y;
+struct Point
+{
+    double x, y;
 };
-void input(struct Point &A){
-	cin >> A.x >> A.y;
+void input(struct Point &A)
+{
+    cin >> A.x >> A.y;
 }
-double distance(struct Point &A, struct Point &B){
-	return sqrt((A.x-B.x)*(A.x-B.x) + (A.y-B.y)*(A.y-B.y));
+double distance(struct Point &A, struct Point &B)
+{
+    return sqrt((A.x - B.x) * (A.x - B.x) + (A.y - B.y) * (A.y - B.y));
 }
-int main(){
-	struct Point A, B;
+int main()
+{
+    struct Point A, B;
     int t;
-	cin>>t;
-    while(t--){
-        input(A); input(B);
-        cout << fixed << setprecision(4) << distance(A,B) << endl;
+    cin >> t;
+    while (t--)
+    {
+        input(A);
+        input(B);
+        cout << fixed << setprecision(4) << distance(A, B) << endl;
     }
     return 0;
 }
