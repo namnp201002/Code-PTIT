664593df416e83d5c2e43205589487625cc17717
namnp201002 <namnp.201002@gmail.com>
2021-12-24 18:12:22 +0700

    struct

--- Struct C/Struct.cpp
+++ Struct C/Struct.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+// bài này a làm theo vào ra file lẫn struct luôn nhé ahihi
 struct SinhVien
 {
     char ten[50], ngaysinh[15];
@@ -30,12 +30,19 @@ int main()
             char a[5];
             itoa(i+1, a, 10);
             strcat(arr[i].MSV, a);
+        }else{
+            char s[10] = "D20DCCN0";
+            strcpy(arr[i].MSV, s);
+            char a[5];
+            itoa(i+1, a, 10);
+            strcat(arr[i].MSV, a);
         }
         fscanf(filein, "\n");
         fgets(arr[i].ten, sizeof(arr[i].ten), filein);
         fscanf(filein, "%s", &arr[i].ngaysinh);
         fscanf(filein, "%f", &arr[i].GPA);
         arr[i].ten[strlen(arr[i].ten) - 1] = '\0';
+        // chỗ này cũng ko hiểu sao trong xâu có enter nên a cho kí tự cuối thành \0 để mất dấu enter
     }
     for(int i = 0; i < so_sinh_vien-1; i++){
         for(int j = i + 1; j < so_sinh_vien; j++){
